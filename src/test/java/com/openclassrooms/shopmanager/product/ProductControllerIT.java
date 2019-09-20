4b249de4017828ab61a08d847a0107a1a078ae13
toastnjamz <veritydavis@gmail.com>
2019-09-19 21:48:22 -0700

    Adding outline for required tests in ProductControllerIT

--- src/test/java/com/openclassrooms/shopmanager/product/ProductControllerIT.java
+++ src/test/java/com/openclassrooms/shopmanager/product/ProductControllerIT.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(ProductController.class)
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -28,6 +29,41 @@ public class ProductControllerIT {
 	private ProductService productService;
 	
 	//@Test
-	//TODO 
+	public void createProduct_missingName_displaysErrorMessage() throws Exception {
+		mockMvc.perform(post("/admin/product")
+				.param("price", "10")
+				.param("quantity", "2"));
+		//TODO
+	}
+	
+	//@Test
+	public void createProduct_missingPrice_displaysErrorMessage() throws Exception {
+		//TODO
+	}
+	
+	//@Test
+	public void createProduct_priceNotANumber_displaysErrorMessage() throws Exception {
+		//TODO
+	}
+	
+	//@Test
+	public void createProduct_priceNotGreaterThanZero_displaysErrorMessage() throws Exception {
+		//TODO
+	}
+	
+	//@Test
+	public void createProduct_MissingQuantity_displaysErrorMessage() throws Exception {
+		//TODO
+	}
+
+	//@Test
+	public void createProduct_QuantityNotAnInteger_displaysErrorMessage() throws Exception {
+		//TODO
+	}
+	
+	//@Test
+	public void createProduct_QuantityNotGreaterThanZero_displaysErrorMessage() throws Exception {
+		//TODO
+	}
 
 }
